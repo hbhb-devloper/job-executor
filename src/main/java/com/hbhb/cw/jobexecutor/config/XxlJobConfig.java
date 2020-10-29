6372ad462f66480731c014d89dd7bfabcd2af702
xiaokang <13148807065@163.com>
2020-10-29 23:54:14 +0800

    修改

--- src/main/java/com/hbhb/cw/jobexecutor/config/XxlJobConfig.java
+++ src/main/java/com/hbhb/cw/jobexecutor/config/XxlJobConfig.java
@@ -14,12 +14,18 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Configuration
 @Slf4j
-public class XxlConfig {
+public class XxlJobConfig {
     @Value("${xxl.job.admin.addresses}")
     private String adminAddresses;
 
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
     @Value("${xxl.job.executor.appname}")
-    private String appName;
+    private String appname;
+
+    @Value("${xxl.job.executor.address}")
+    private String address;
 
     @Value("${xxl.job.executor.ip}")
     private String ip;
@@ -27,21 +33,20 @@ public class XxlConfig {
     @Value("${xxl.job.executor.port}")
     private int port;
 
-    @Value("${xxl.job.accessToken}")
-    private String accessToken;
-
     @Value("${xxl.job.executor.logpath}")
     private String logPath;
 
     @Value("${xxl.job.executor.logretentiondays}")
     private int logRetentionDays;
 
+
     @Bean
     public XxlJobSpringExecutor xxlJobExecutor() {
         log.info(">>>>>>>>>>> xxl-job config init.");
         XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
         xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
-        xxlJobSpringExecutor.setAppname(appName);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAddress(address);
         xxlJobSpringExecutor.setIp(ip);
         xxlJobSpringExecutor.setPort(port);
         xxlJobSpringExecutor.setAccessToken(accessToken);
