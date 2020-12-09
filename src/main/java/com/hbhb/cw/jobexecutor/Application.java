861cf280d44e9be214a5ac1119917987f2512c01
xiaokang <648638730@qq.com>
2020-12-09 16:38:38 +0800

    修改配置

--- src/main/java/com/hbhb/cw/jobexecutor/Application.java
+++ src/main/java/com/hbhb/cw/jobexecutor/Application.java
@@ -1,20 +1,19 @@
 package com.hbhb.cw.jobexecutor;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author dxk
  */
-@RefreshScope
-@EnableScheduling
 @EnableFeignClients
 @SpringCloudApplication
 @ComponentScan("com.hbhb")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
